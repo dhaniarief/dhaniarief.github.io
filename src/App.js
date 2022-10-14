670d46eed92abd48297eff1e69effd323f066ded
dhaniarief <dhani.arief1995@gmail.com>
2022-10-14 12:53:31 +0700

    belajar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing Testing Testing
+          Masih Belajar
         </a>
       </header>
     </div>
